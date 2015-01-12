a8dbe8502b6b49b4d64a0f37f5f0a3d5be3b993b
ArtturiSipila <artturi.sipila@gmail.com>
2015-01-12 14:52:40 +0200

    Some tweaking: added a stub for $destroy and some styling etc.

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,105 +1,117 @@
-/**
- * Created by Artturi Sipilä on 24.9.2014.
- */
-
-
 angular.module('autocomplete', [] )
     .directive('autocomplete', function ($parse, $http, $sce, $timeout) {
         return {
             restrict: 'EA',
             templateUrl: 'src/autocompleteTemplate.html',
             scope: {}, // http://stackoverflow.com/questions/14050195/what-is-the-difference-between-and-in-directive-scope
-            link: function($scope, element, attrs) {
+            link: function(scope, element, attrs) {
                 var input = element.find('input');
                 var browseBtn = element.find('button'); //change this to an id selector if we have more buttons
 
+                scope.showDropdown = false;
                 //function declarations
 
-                $scope.keyPressed = function(event) {
+                scope.keyPressed = function(event) {
                     if (!(event.which == 38 || event.which == 40 || event.which == 13)) {
 
-                        $scope.showDropdown = true;
-                        /*
+                        scope.showDropdown = true;
 
-                        if (!$scope.searchStr || $scope.searchStr == "") {
-                            $scope.showDropdown = false;
-                            $scope.lastSearchTerm = null
-                        } else if (isNewSearchNeeded($scope.searchStr, $scope.lastSearchTerm)) {
-                            $scope.lastSearchTerm = $scope.searchStr
-                            $scope.showDropdown = true;
-                            $scope.currentIndex = -1;
-                            $scope.results = [];
 
-                            if ($scope.searchTimer) {
-                                $timeout.cancel($scope.searchTimer);
+                        if (!scope.queryString || scope.queryString == "") {
+                            console.log("Empty search string");
+                            scope.showDropdown = false;
+                            scope.lastSearchTerm = null
+
+
+                        } else if (isNewSearchNeeded(scope.queryString, scope.lastSearchTerm)) { //TODO: rename to : found in cache etc.
+                            console.log("Search needed");
+                            scope.lastSearchTerm = scope.queryString;
+                            scope.showDropdown = true;
+                            scope.currentIndex = -1;
+                            scope.results = [];
+
+                            /*
+                            if (scope.searchTimer) {
+                                $timeout.cancel(scope.searchTimer);
                             }
+                            */
 
-                            $scope.searching = true;
+                            scope.searching = true;
 
-                            $scope.searchTimer = $timeout(function() {
-                                $scope.searchTimerComplete($scope.searchStr);
-                            }, $scope.pause);
+                            /*
+                            scope.searchTimer = $timeout(function() {
+                                scope.searchTimerComplete(scope.queryString);
+                            }, scope.pause);
+                            */
                         }
-                        */
+
                     } else {
                         event.preventDefault();
                     }
                 };
 
+                //TODO
+                var isNewSearchNeeded = function(a, b) {
+                    return true;
+                };
 
-                $scope.browse = function(event) {
+                scope.browse = function(event) {
                     console.log("Browse!");
-                }
+                };
 
                 //Bind event listeners
 
-
-                input.on('keyup', $scope.keyPressed);
-                browseBtn.on('click', $scope.browse);
+                input.on('keyup', scope.keyPressed);
+                browseBtn.on('click', scope.browse);
 
                 element.on("keyup", function (event) {
 
                     /*
                     if(event.which === 40) {
-                        if ($scope.results && ($scope.currentIndex + 1) < $scope.results.length) {
-                            $scope.currentIndex ++;
-                            $scope.$apply();
+                        if (scope.results && (scope.currentIndex + 1) < scope.results.length) {
+                            scope.currentIndex ++;
+                            scope.$apply();
                             event.preventDefault;
                             event.stopPropagation();
                         }
 
-                        $scope.$apply();
+                        scope.$apply();
                     } else if(event.which == 38) {
-                        if ($scope.currentIndex >= 1) {
-                            $scope.currentIndex --;
-                            $scope.$apply();
+                        if (scope.currentIndex >= 1) {
+                            scope.currentIndex --;
+                            scope.$apply();
                             event.preventDefault;
                             event.stopPropagation();
                         }
 
                     } else if (event.which == 13) {
-                        if ($scope.results && $scope.currentIndex >= 0 && $scope.currentIndex < $scope.results.length) {
-                            $scope.selectResult($scope.results[$scope.currentIndex]);
-                            $scope.$apply();
+                        if (scope.results && scope.currentIndex >= 0 && scope.currentIndex < scope.results.length) {
+                            scope.selectResult(scope.results[scope.currentIndex]);
+                            scope.$apply();
                             event.preventDefault;
                             event.stopPropagation();
                         } else {
-                            $scope.results = [];
-                            $scope.$apply();
+                            scope.results = [];
+                            scope.$apply();
                             event.preventDefault;
                             event.stopPropagation();
                         }
 
                     } else if (event.which == 27) {
-                        $scope.results = [];
-                        $scope.showDropdown = false;
-                        $scope.$apply();
+                        scope.results = [];
+                        scope.showDropdown = false;
+                        scope.$apply();
                     } else if (event.which == 8) {
-                        $scope.selectedObject = null;
-                        $scope.$apply();
+                        scope.selectedObject = null;
+                        scope.$apply();
                     }
                     */
             });
+
+                //TODO: cleanup
+                element.on('$destroy', function() {
+                    //$interval.cancel(timeoutId);
+                });
         }
         }
     });
