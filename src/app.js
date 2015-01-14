3ea7c9a1bfa6d0c5eef8cf40f98e045f553c8921
ArtturiSipila <artturi.sipila@gmail.com>
2015-01-14 13:18:33 +0200

    add tests

---
+++ src/app.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Artturi Sipilä on 24.9.2014.
+ */
+
+var test_app = angular.module('testapp', ["autocomplete"]);
+
+/*
+test_app.run(function($templateCache){
+    $templateCache.put("autocomplete.html", '<div></div>')
+});
+*/
+
+test_app.controller('TestController', ['$scope',
+        function TestController($scope) {
+            $scope.searchFieldsInData = ["color"];
+           $scope.colors = [
+                {
+                    color: "red",
+                    value: "#f00"
+                },
+                {
+                    color: "green",
+                    value: "#0f0"
+                },
+                {
+                    color: "blue",
+                    value: "#00f"
+                },
+                {
+                    color: "cyan",
+                    value: "#0ff"
+                },
+                {
+                    color: "magenta",
+                    value: "#f0f"
+                },
+                {
+                    color: "yellow",
+                    value: "#ff0"
+                },
+                {
+                    color: "black",
+                    value: "#000"
+                }
+            ];
+
+        }
+    ]);
